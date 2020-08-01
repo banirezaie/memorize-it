ebb66ec20c863639a4ae2eae2c3d842a8474ede4
BANI GIT <bani.rezaie@gmail.com>
2020-08-01 23:14:29 +0100

    transition

--- src/FlitCard.js
+++ src/FlitCard.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const FlipCard = () => {
-  let chosenCard = [];
-  let chosenCardId = [];
+  //   let chosenCard = [];
+  //   let chosenCardId = [];
 
   return (
     <div>
